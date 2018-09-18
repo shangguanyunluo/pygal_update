249df6210e8db988404dde8d5af0eb1e6552c976
shangguanyunluo <zhang.yafeng@foxmail.com>
2018-09-19 00:39:35 +0800

    add new files

--- pygal2_update/python_repos4.py
+++ pygal2_update/python_repos4.py
@@ -1,34 +1,36 @@
-# encoding: utf-8
-'''
-Created on 2018年9月18日
-
-@author: Administrator
-'''
 import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
-
+# Make an API call, and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
+print("Status code:", r.status_code)
 
-print("status code:", r.status_code)
-
+# Store API response in a variable.
 response_dict = r.json()
-print(response_dict.keys())
+print("Total repositories:", response_dict['total_count'])
 
-print("Total repositories:", response_dict["total_count"])
-
+# Explore information about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
 names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    plot_dict = {'value':repo_dict['stargazers_count'],
-                 'label':repo_dict['description'], }
+    label = repo_dict['description'] if repo_dict['description'] is not None else "aaaa"
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': label,
+        'xlink': repo_dict['html_url'],
+        }
+    print('value', repo_dict['stargazers_count'],
+        'label', repo_dict['description'],
+        'xlink', repo_dict['html_url'])
     plot_dicts.append(plot_dict)
-    
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
 my_config.show_legend = False
@@ -38,12 +40,11 @@ my_config.major_label_font_size = 18
 my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
-    
-my_style = LS('#333366', base_style=LCS)
+
 chart = pygal.Bar(my_config, style=my_style)
-chart.title = "Most-Starred Python Projects on GitHub"
+chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
-chart.add("", plot_dicts)
-chart.render_to_file("python_repos2.svg")
 
-
+chart.add('', plot_dicts)
+# chart.render_in_browser()
+chart.render_to_file(filename='python_repos2.svg', kwards="aaa")
